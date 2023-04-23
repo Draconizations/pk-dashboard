105177768fca5869072d65c2e76561270e0e3135
Jake Fulmine <jake@fulmine.xyz>
2023-04-23 19:39:55 +0200

    tweak: link to commit in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,11 +4,12 @@
     import { Footer, FooterLink, FooterLinkGroup } from "flowbite-svelte";
 
     const repoUrl = PUBLIC_REPOSITORY_URL || "https://github.com/pluralkit/pluralkit/"
+    const commitHash = version.split("-")[1]
 </script>
 
 <Footer class="shadow-none mt-auto flex dark:bg-gray-900" >
     <FooterLinkGroup ulClass="flex flex-wrap items-center justify-end flex-1 mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <FooterLink href={repoUrl}>
+        <FooterLink href={`${repoUrl}/commit/${commitHash}`}>
             {version}
         </FooterLink>
         <FooterLink href="https://pluralkit.me/privacy/">
