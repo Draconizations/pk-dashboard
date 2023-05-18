4355278303fb9e58801c97813e87f892379faebb
Jake Fulmine <jake@fulmine.xyz>
2023-05-18 09:44:26 +0200

    move member list layout to page

--- src/routes/(dash)/dash/(list)/members/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import MemberListControl from "$lib/components/MemberListControl.svelte";
-</script>
-
-<div class="w-full lg:w-1/2 xl:w-1/3">
-    <MemberListControl />
-</div>
---
+++ src/routes/(dash)/dash/(list)/members/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import MemberListControl from "$lib/components/member/MemberListControl.svelte";
+</script>
+
+<div class="w-full lg:w-1/2 xl:w-1/3">
+    <MemberListControl />
+    <slot/>
+</div>
